0f5d2ca846c24b27d0077d694b7d8d441b41d85f
Chaneshm <chaneshm0812@gmail.com>
2022-11-04 13:48:43 -0400

    update bin repo

--- bin/autojenkins.sh
+++ bin/autojenkins.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 if [ $UID != 0 ]; then
     echo "Run again with admin permissions"
     exit 1
@@ -9,18 +7,6 @@ sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.i
 apt-get update
 #Install java, Jenkins, pip and venv in that order
 apt-get install default-jre -y && echo "Installed Java Runtime Engine" && apt-get install jenkins -y && echo "Installed Jenkins" && apt-get install python3-pip -y && echo "Installed Python pip" && apt-get install python3.10-venv -y && echo "Installed Python venv"
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
-wget -O- https://apt.releases.hashicorp.com/gpg | \
-    gpg --dearmor | \
-    sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-gpg --no-default-keyring \
-    --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
-    --fingerprint
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
-    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
-    sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
-sudo apt-get install terraform
 #Start the Jenkins service
 systemctl start jenkins && echo "Jenkins Started"
 #successful
